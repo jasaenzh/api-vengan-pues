702581ede6641241059ec5578b19c7dd838b2312
Jhony Saenz <jash0310@gmail.com>
2023-05-01 19:34:15 -0500

    inicio de sesion usando next-auth github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,61 @@
-import { getSession } from 'next-auth/react'
-import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { useRouter } from "next/router";
+
+interface IUser {
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
+}
 
 function IndexPage() {
-  useEffect(() => {
-    // Creo una funcion que se invoca sola
-    (async () => {
-      const session = await getSession()
-      console.log(session);
-    })()
-    console.log("Cargo");
-  }, [])
+  const { data: session, status } = useSession();
+  const user: IUser = session?.user ?? {};
+
+  const router = useRouter();
+
+  console.log("session:", session, "status:", status);
+
+  // Cargando aplicacion
+  if (status === 'loading') {
+    return (
+      <div>
+        <p>...cargando</p>
+      </div>
+    )
+  }
+
+  // Si no esta logueado lo envio a login
+  if (status === 'unauthenticated') {
+    router.push('/login')
+  }
+
+  // Renderizado
   return (
     <div>
-      <h1>Estoy en el Index</h1>
+      {
+        // Valido si vienen datos
+        session ?
+          // Si vienen datos muestro lo siguiente
+          (
+            <div>
+              <h1>Bienvenido, {user?.name}</h1>
+              {user?.image && (
+                <Image src={user.image} alt={user.name || ''} width={40} height={40} />
+              )}
+
+            </div>
+          )
+          : (
+            // Si no hay datos muestro esto pero lo redirigue a Login
+            <div>
+
+              <h1>redireccionando a login</h1>
+
+            </div>
+          )
+      }
+
     </div>
   );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,53 +1,12 @@
-import React, { useState } from 'react';
-import axios, { AxiosResponse, AxiosError } from 'axios';
-import { useRouter } from 'next/router';
-
+import { useRouter } from "next/router";
 
 function LoginPage() {
-
-  // Estado que guarda las credenciales emial y contraseña
-  const [credenciales, setCredenciales] = useState({
-    email: '',
-    password: ''
-  })
-
-  const router = useRouter()
-
-  // Funcion que captura los valores del input
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-
-    // El evento target.value es el valor que se va tipeando en el input
-    // El evento target.name es atributo name del input
-    setCredenciales({
-      ...credenciales,
-      [event.target.name]: event.target.value
-    })
-  }
-
-  interface Login {
-    email: string,
-    password: string
-  }
-
-  // Funcion para enviar los datos a la BD (prevent default detiene el comportamiento del form)
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const response = await axios.post<Login>('/api/auth/login', credenciales)
-
-    if (response.status === 200) {
-      router.push('/dashboard')
-    }
-
-  }
-
+  const router = useRouter();
   return (
     <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="email" name="email" id="email" placeholder='email' onChange={handleChange} />
-        <input type="password" name="password" id="password" placeholder='password' onChange={handleChange} />
-        <button>Login</button>
-      </form>
+      <button onClick={() => router.push('/api/auth/signin/github')}>
+        Inicio con github
+      </button>
     </div>
   )
 }
